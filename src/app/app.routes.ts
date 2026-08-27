999be9b0474d0773b87628d2116d167f073526b3
KMELZB <melvynb2904@outlook.fr>
2026-08-27 15:06:48 +0200

    Configurer routing et environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { WeatherComponent } from './pages/weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'weather/:city', component: WeatherComponent },
+];
